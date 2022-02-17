efa6e4bf41dd75d027624ee4453cde87f083224d
Fahri Maulana <88439222+fahrimaulanaa@users.noreply.github.com>
2022-02-17 15:40:44 +0700

    initial commit

--- Enkap1/Enkapsulasi1.java
+++ Enkap1/Enkapsulasi1.java
@@ -5,10 +5,14 @@ public class Enkapsulasi1{
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        //memanggil private method lpersegi
+        System.out.println("Keliling persegi: ");
+        System.out.println("Rumus: s * 4");
         kpersegi();
-        //Memanggil private method harus di class yang sama
     }
 
+    //Public method yang akan diakses oleh Enkap22
     public void lpersegi() {
         System.out.println("Masukkan panjang sisi: ");
         int sisi = sc.nextInt();
@@ -16,6 +20,7 @@ public class Enkapsulasi1{
         System.out.println("Luas persegi: "+luas);
     }
 
+    //protected method yang akan diakses oleh Enkap22 dan pemanggilan
     protected void lpersegipanjang(){
         System.out.println("Masukkan Panjang: ");
         int panjang = sc.nextInt();
@@ -25,10 +30,20 @@ public class Enkapsulasi1{
         System.out.println("Luas persegi panjang: "+luas);
     }
 
+    //private method yang akan diakses oleh pemanggilan
     private static void kpersegi() {
         System.out.println("Masukkan panjang sisi: ");
         int sisi = sc.nextInt();
         int keliling = 4 * sisi;
         System.out.println("Keliling persegi: "+keliling);
+    }
+
+    //public method yang akan diakses oleh Enkap22
+    public void luaslingkaran(){
+        double phi = 3.14;
+        System.out.println("Masukkan jari jari: ");
+        double jarijari = sc.nextDouble();
+        double luas = phi * jarijari * jarijari;
+        System.out.println("Luas lingkaran: "+luas);
     }
 }
--- Enkap2/Enkap22.java
+++ Enkap2/Enkap22.java
@@ -5,8 +5,15 @@ import Enkap1.Enkapsulasi1;
 public class Enkap22 {
     public static void main(String[] args) {
         Enkapsulasi1 enkapsulasi1 = new Enkapsulasi1();
+        Enkapsulasi1 enkapsulasi4 = new Enkapsulasi1();
 
+        //memanggil public method dari package Enkap1 dan class Enkapsulasi1 pada method lpersegi
+        System.out.println("Luas persegi: ");
         enkapsulasi1.lpersegi();
-        //memanggil public method dari package dan class yang berbeda
+        System.out.println("\n");
+
+        //memanggil public method dari package Enkap1 dan class Enkapsulasi1 pada method lpersegipanjang
+        System.out.println("Luas persegi panjang: ");
+        enkapsulasi4.luaslingkaran();       
     }
 }
